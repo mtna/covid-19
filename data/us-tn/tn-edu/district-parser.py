f268ed42fc126a435c9be4eeba28272178072de7
Andrew <andrew.decarlo@mtna.us>
2020-10-22 08:40:12 -0400

    added district level counts in case we need them in the future.

--- data/us-tn/tn-edu/district-parser.py
+++ data/us-tn/tn-edu/district-parser.py
@@ -9,16 +9,21 @@ if __name__ == "__main__":
     if response.status_code != 200:
         print("Request failed ["+response.status_code+"]")
 
+    # date collected
+    today = datetime.date(datetime.now()).isoformat()
+
     # add the results to a data frame
-    df = pd.DataFrame(columns = ['district_id', 'district_name', 'region_id', 'region_name']) 
+    df = pd.DataFrame(columns = ['district_id', 'district_name', 'region_id', 'region_name', 'student_cases', 'staff_cases', 'date_stamp']) 
     districtArr = response.json()
     for district in districtArr:
         region = district['region']
-        df = df.append({'district_id' : district['id'], 'district_name' : district['name'], 'region_id' : region['id'], 'region_name' : region['name']}, ignore_index = True)
+        covid = district['covidData']
+        df = df.append({'district_id' : district['id'], 'district_name' : district['name'], 'region_id' : region['id'], 'region_name' : region['name'], 'student_cases' : covid['studentCases'], 'staff_cases' : covid['staffCases'], 'date_stamp' : today}, ignore_index = True)
 
     df = df.sort_values(by=['district_id','region_id'], ascending=True)
     
-    # write the data frame to a dated snapshot
+    # write the data frame to a dated snapshot and to latest
     date = datetime.now().strftime('%Y-%m-%d')
     snapshot = './data/us-tn/tn-edu/raw/districts-'+date+'.csv'
     df.to_csv(f'{snapshot}', index=False)
+    df.to_csv(f'./data/us-tn/tn-edu/raw/districts-latest.csv', index=False)
