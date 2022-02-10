4c1be0071301f3cffa6cbf7c3a64bd896f1763f9
Andrew <andrew.decarlo@mtna.us>
2022-02-10 08:22:57 -0500

    Excluded Antarctica and Winter Olympics 2022 as these are not valid countries

--- data/us/jhu-ccse/harvester.py
+++ data/us/jhu-ccse/harvester.py
@@ -144,7 +144,7 @@ def get_source_df(report_date):
     df = pd.read_csv(df_url, names=df_names, dtype=df_dtypes, header=0)
     # drop non-standard entities
     df = df[(df.country_name != 'Diamond Princess') & (df.country_name != 'Cruise Ship') & (
-        df.country_name != 'MS Zaandam') & (df.country_name != 'Channel Islands') & (df.country_name != 'Others') & (df.country_name != 'Summer Olympics 2020')] 
+        df.country_name != 'MS Zaandam') & (df.country_name != 'Channel Islands') & (df.country_name != 'Others') & (df.country_name != 'Summer Olympics 2020') & (df.country_name != 'Antarctica') & (df.country_name != 'Winter Olympics 2022')] 
     # remove leading/trailing spaces
     df.country_name = df.country_name.str.strip()
     return df
